e820e064159ca4cdba2acf7e546ce7f068f53868
Neelam B V D Soujitha <94949334+soujitha234@users.noreply.github.com>
2023-07-14 22:43:48 +0530

    Create assignment_03.java

---
+++ assignment_03.java
@@ -0,0 +1,276 @@
+1)
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) {
+                    return sum;
+                }
+
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        ThreeSumClosest solution = new ThreeSumClosest();
+        int closestSum = solution.threeSumClosest(nums, target);
+        System.out.println(closestSum);
+    }
+}
+
+2)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return quadruplets;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        FourSum solution = new FourSum();
+        List<List<Integer>> quadruplets = solution.fourSum(nums, target);
+        System.out.println(quadruplets);
+    }
+}
+
+3)
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        NextPermutation solution = new NextPermutation();
+        solution.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
+4)
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        SearchInsertPosition solution = new SearchInsertPosition();
+        int index = solution.searchInsert(nums, target);
+        System.out.println(index);
+    }
+}
+
+5)
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        // If all digits are 9, create a new array with an additional digit
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        PlusOne solution = new PlusOne();
+        int[] result = solution.plusOne(digits);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+6)
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+        SingleNumber solution = new SingleNumber();
+        int single = solution.singleNumber(nums);
+        System.out.println(single);
+    }
+}
+
+7)
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingRanges {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> missingRanges = new ArrayList<>();
+        long start = lower;
+
+        for (int num : nums) {
+            if (num > start) {
+                missingRanges.add(getRange(start, num - 1));
+            }
+            start = (long) num + 1;
+        }
+
+        if (start <= upper) {
+            missingRanges.add(getRange(start, upper));
+        }
+
+        return missingRanges;
+    }
+
+    private String getRange(long start, long end) {
+        if (start == end) {
+            return String.valueOf(start);
+        } else {
+            return start + "->" + end;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 3, 50, 75};
+        int lower = 0;
+        int upper = 99;
+        MissingRanges solution = new MissingRanges();
+        List<String> ranges = solution.findMissingRanges(nums, lower, upper);
+        System.out.println(ranges);
+    }
+}
+
+8)
+import java.util.Arrays;
+
+public class MeetingRooms {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
+        MeetingRooms solution = new MeetingRooms();
+        boolean canAttendAll = solution.canAttendMeetings(intervals);
+        System.out.println(canAttendAll);
+    }
+}
